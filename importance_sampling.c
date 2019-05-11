d8bd2d0ad2a0b98df29c9f9ecac6ab67bfc7d61e
Fernando Paz <ferpaz9292@gmail.com>
2019-05-11 17:03:27 -0300

    Agrego comentarios

--- importance_sampling.c
+++ importance_sampling.c
@@ -23,7 +23,7 @@ int main(int argc,char *argv[]){
 	sscanf(argv[1], "%d", & N);
 
 	a=integral(0.5,N, 0.6);
-	delta_optimo(0.5, N, 0.6);
+	delta_optimo(0.5, N, 0.6);   
 
 	printf("\nEl valor de la integral es:  %f \n", a);	
 	return 0;
@@ -33,28 +33,23 @@ int main(int argc,char *argv[]){
 
 
 
-float random_walk(float x,float delta){
+float random_walk(float x,float delta){                    //me dice el proximo punto en una ventana de delta
 	float a;
 	a=aleatorio();
 	return x+(a-0.5)*delta;	
-}
+}	
 
-float aleatorio(void){
+float aleatorio(void){					//devuelve un numero aeatorio entre 0 y 1
 	return ((float)rand())/((float)RAND_MAX);
 }
 
 
-float dist(float x){
+float dist(float x){					//esta es la distriubución a estudiar
 	float a;
 	a=exp(-(pow(x,2))/2);
 	return a;
 }
 
-int proximo_x(float x, float delta){
-	float a;
-	a=random_walk(x,delta);
-	return a;
-}
 
 
 
@@ -64,8 +59,8 @@ float muestreo(float *x, int N, float delta,float *suma){
 	int i;
 	float p, nuevo_x,a;
 	i=0;
-	FILE *fpb= fopen("muestreo", "a");
-	int aceptados = 0;
+	FILE *fpb= fopen("muestreo", "a");            //Abre un archivo ya creado y vuelca mas información adentro
+	int aceptados = 0;			      //para una nueva medición es conveniente borrar el archivo a menos que se quira acumular 								datos (para hacer esto ultimo no hay que cambiar N la longitud del muestro)
 	
 	*suma=0;
 	
@@ -75,12 +70,12 @@ float muestreo(float *x, int N, float delta,float *suma){
 
 		a=pow(nuevo_x,2)-pow(*(x+i),2);		
 		
-		if(a<0){
+		if(a<0){				//si a<0 la exponencial es mayor a 1 entonces acepto con probabilidad 1
 			*(x+i+1)=nuevo_x;
 			aceptados++;
 		}
 		else{
-			p=dist(nuevo_x)/dist(*(x+i))/(2*delta);
+			p=exp(-a/2)/(2*delta);   
 			if(aleatorio()<p){
 				*(x+i+1)=nuevo_x;
 				aceptados++;		
@@ -92,7 +87,7 @@ float muestreo(float *x, int N, float delta,float *suma){
 			
 
 		i++;	
-		fprintf(fpb, " %f", nuevo_x);
+		fprintf(fpb, " %f", nuevo_x);         //para cada delta guarda la totalidad de los pasos (aceptados o no)
 	}
 	for(i=0;i<N;i++){
 		*suma=*suma+pow(*(x+i),2)/N;
@@ -106,7 +101,7 @@ return ((float)aceptados)/((float)i);
 }
 
 
-float integral(float x_ini,int N, float delta){
+float integral(float x_ini,int N, float delta){     //calcula la integral una unica vez
 	float *suma, *x;
 	
 	x=(float*)malloc(N*sizeof(float));
@@ -126,8 +121,8 @@ float delta_optimo(float x_ini,int N, float delta){
 	float *delt,paso_delta,delta_inicio,delta_fin,*suma,*x;	
 	int i,long_delta;	
 	
-	delta_inicio=1.4;         //arrancar en 1.4
-	delta_fin=1.5;		//finalizar en 1.5  esto garantiza probabilidad de aceptacion aproximadamente 1/2 (dist gaussiana de ptos).
+	delta_inicio=0;         //arrancar en 1.4
+	delta_fin=10;		//finalizar en 1.5  esto garantiza probabilidad de aceptacion aproximadamente 1/2 (dist gaussiana de ptos).
 	long_delta=100;
 	paso_delta=((float)(delta_fin-delta_inicio))/((float)long_delta);
 
@@ -144,7 +139,7 @@ float delta_optimo(float x_ini,int N, float delta){
 		
 	*x=x_ini;	
 	
-	for(i=0;i<long_delta;i++){
+	for(i=0;i<long_delta;i++){					//barre en delta e imprime delta, tasa de aceptación y resultado
 		fprintf(fdelta, "%f %f %f \n", *(delt+i), muestreo(x, N,*(delt+i),suma), *suma);
 	}
 		
